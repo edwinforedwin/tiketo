636c9d8a95931121c3d1715aac0060e8a482764e
edwinforedwin <edwinforedwin@gmail.com>
2024-11-30 00:00:48 +0530

    userhome changes

--- src/app/components/admin/adminhome/adminhome.component.ts
+++ src/app/components/admin/adminhome/adminhome.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-adminhome',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet,MenubarModule],
   templateUrl: './adminhome.component.html',
   styleUrl: './adminhome.component.css'
 })
-export class AdminhomeComponent {
-
+export class AdminhomeComponent implements OnInit {
+  menuItems:MenuItem[]=[]
+  ngOnInit() {
+    this.menuItems = [
+      {label:'Home',icon:'pi pi-home'},
+      {label:'User Management',icon:'pi pi-user',items:[
+        {label:'User Creation',icon:'pi pi-user-edit',routerLink:'usercreation'},
+        {label:'User Modification',icon:'pi pi-user-edit'}
+      ]},
+      {label:'Reports',icon:'pi pi-folder',items:[
+        {label:'User Review Report',icon:'pi pi-file'}
+      ]}
+    ]
+  }
 }
