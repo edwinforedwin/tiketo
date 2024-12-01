ad47198f7749d1ceee79d09223fcef3fe4be718a
edwinforedwin <edwinforedwin@gmail.com>
2024-12-01 21:25:15 +0530

    usercreation changes

--- src/app/components/admin/usercreation/usercreation.component.ts
+++ src/app/components/admin/usercreation/usercreation.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-usercreation',
   standalone: true,
-  imports: [InputTextModule,FloatLabelModule],
+  imports: [InputTextModule,FloatLabelModule,ButtonModule],
   templateUrl: './usercreation.component.html',
   styleUrl: './usercreation.component.css'
 })
